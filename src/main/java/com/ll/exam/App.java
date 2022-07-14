55366df4b36adb51a9f6d3b766d595c4d53f097f
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-14 15:05:29 +0900

    3단계

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class App {
     Scanner sc;
+    int wisesayingfindid;
     App() {
         sc = new Scanner(System.in);
+        wisesayingfindid = 0;
     }
     public void run() {
         System.out.println("=== 명언 SSG ===");
@@ -19,10 +21,12 @@ public class App {
                 case "종료":
                     break outer;
                 case "등록":
+                    ++wisesayingfindid;
                     System.out.print("명언 : ");
                     String content = sc.nextLine().trim();
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
+                    System.out.println(wisesayingfindid + "번 명언이 등록되었습니다.");
             }
         }
     }
