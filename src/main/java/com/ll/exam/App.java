38c65f935b40e38f60631558a82da9359ea4745f
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-14 14:42:13 +0900

    명령 프롬프트 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,10 +1,23 @@
 package com.ll.exam;
 
-public class App {
-    App() {
+import java.util.Scanner;
 
+public class App {
+    Scanner sc;
+    String cmd;
+    App() {
+        sc = new Scanner(System.in);
     }
     void run() {
+        System.out.println("=== 명언 SSG ===");
+        while(true){
+            cmd = sc.nextLine().trim();
+            outer:
+            switch (cmd){
+                case "종료":
+                    break outer;
+            }
+        }
 
     }
 }
