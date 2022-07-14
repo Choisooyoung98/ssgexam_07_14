da6ffc00e42d930de786778b59df652a342dc605
Choisooyoung98 <ekfms15815@gmail.com>
2022-07-14 15:17:48 +0900

    목록 5단계 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,13 +1,18 @@
 package com.ll.exam;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     Scanner sc;
     int wisesayingfindid;
+    List<WiseSaying> wiseSayings;
     App() {
         sc = new Scanner(System.in);
         wisesayingfindid = 0;
+        wiseSayings = new ArrayList<>();
     }
     public void run() {
         System.out.println("=== 명언 SSG ===");
@@ -22,11 +27,28 @@ public class App {
                     break outer;
                 case "등록":
                     ++wisesayingfindid;
+
                     System.out.print("명언 : ");
                     String content = sc.nextLine().trim();
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
+
+                    WiseSaying wiseSaying = new WiseSaying(wisesayingfindid, content, author);
+                    wiseSayings.add(wiseSaying);
                     System.out.println(wisesayingfindid + "번 명언이 등록되었습니다.");
+                    System.out.println(wiseSaying.toString());
+                    continue;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("------------------");
+
+                    WiseSaying wiseSaying_;
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--){
+                        wiseSaying_ = wiseSayings.get(i);
+                        System.out.println(wiseSaying_.id + " / " + wiseSaying_.content + " / " + wiseSaying_.author);
+                    }
+                    continue;
+
             }
         }
     }
